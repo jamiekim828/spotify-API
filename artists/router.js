09034d5f9da290a9cbd73a7e846479aca0002ac9
jamiekim828 <jamiekim828@gmail.com>
2019-06-29 17:52:48 +0200

    artist router.js add

--- artists/router.js
+++ artists/router.js
@@ -1,9 +1,16 @@
 const { Router } = require('express')
 const auth = require('../auth/middleware')
+const playlists = require('../playlists/model')
 const router = new Router()
 
-router.get('/artists', (req, res, next) => {
-
+router.get('/artists', auth, (req, res, next) => {
+    playlists.findAll({
+        where: { userId: req.user.id },
+        include: [Songs]
+    })
+    .then(res => console.log(res))
+    .then(res => res.status(200).send({ message: 'Valid contents'}))
+    .catch(err => {res.status(404).json(err)})
 })
 
 router.put('/playlists/:id/songs/:id', (req, res, next) => {
